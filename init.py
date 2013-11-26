9a1a1054323273f8b18a89b2bbfc93b5bdf7e3b1
parvathyraveendran <parvathyraveendran22@gmail.com>
2013-11-26 10:01:47 +0530

    Create init.py

---
+++ init.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_syncdb
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import models as auth_models
+from django.contrib.auth.models import Permission
+
+# custom user related permissions
+def add_user_permissions(sender, **kwargs):
+    ct = ContentType.objects.get(app_label='auth', model='user')
+    perm, created = Permission.objects.get_or_create(codename='can_view', name='Can View Users', content_type=ct)
+def add_user_permissions(sender, **kwargs):
+    ct = ContentType.objects.get(app_label='auth', model='user')
+    perm, created = Permission.objects.get_or_create(codename='can_page', name='Can View pages', content_type=ct)
+def add_user_permissions(sender, **kwargs):
+    ct = ContentType.objects.get(app_label='auth', model='user')
+    perm, created = Permission.objects.get_or_create(codename='can_polls', name='Can View polls', content_type=ct)
+
+post_syncdb.connect(add_user_permissions, sender=auth_models)
